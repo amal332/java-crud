cd3cbdd8b1a4399ad868370249ce5ab009c138ec
amal332 <144212326+amal332@users.noreply.github.com>
2023-09-07 09:58:37 +0530

    Add files via upload

---
+++ demo/src/main/java/com/example/crudApplication/demo/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.example.crudApplication.demo.repository;
+
+import com.example.crudApplication.demo.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+}
